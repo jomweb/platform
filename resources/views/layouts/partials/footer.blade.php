157d21e8dac48dc450ca73301c58b9f3e9a0dd58
crynobone <crynobone@gmail.com>
2014-12-12 11:45:00 +0800

    Improves footer.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2 centered">
-                <h5>Hosted with <a href="https://www.digitalocean.com/?refcode=e1f9dab486bc">DigitalOcean</a>.</h5>
-                <p class="mt">
-                    <a href="#"><i class="ion-social-facebook"></i></a>
-                    <a href="#"><i class="ion-social-github"></i></a>
-                </p>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2 centered">
+				<h5>Developed with <a href="http://orchestraplatform.com" target="_blank">Orchestra Platform</a> and hosted with <a href="https://www.digitalocean.com/?refcode=e1f9dab486bc" target="_blank">DigitalOcean</a>.</h5>
+				<p class="mt">
+					<a href="https://www.facebook.com/groups/jomweb" target="_blank"><i class="ion-social-facebook"></i></a>
+					<a href="https://github.com/jomweb" target="_blank"><i class="ion-social-github"></i></a>
+				</p>
+			</div>
+		</div>
+	</div>
 </footer>
