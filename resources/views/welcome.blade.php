295de80aadef89b7d0413d07cb11312a0d7bfb80
crynobone <crynobone@gmail.com>
2014-12-12 11:51:11 +0800

    Add basic welcome.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+
+<section id="home"></section>
+<div id="h">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<h1>JOMWEB merupakan satu inisiatif yang digerakkan oleh komuniti pembangun tempatan dalam mengangkat darjat dan menceriakan lagi industri IT di Malaysia.</h1>
+			</div>
+		</div>
+	</div><!-- /.container -->
+</div><! --/H -->
+
 @stop
