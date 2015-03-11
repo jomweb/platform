c30c2c74544a4119b7f2016468e4617186a0acc9
crynobone <crynobone@gmail.com>
2015-03-11 11:50:08 +0800

    Initial commit.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ build/subsplit-basic.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -d .subsplit ]; then
+    git subsplit update
+else
+    git subsplit init git@github.com:jomweb/platform.git
+fi
+
+git subsplit publish --heads="master develop" --no-tags extensions/diskus:git@github.com:orchestral/diskus.git
+git subsplit publish --heads="master develop" --no-tags extensions/shorty:git@github.com:orchestral/shorty.git
