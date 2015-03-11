be7c4abcbdb5267f7215a60f681d0b4d1ed600e4
crynobone <crynobone@gmail.com>
2015-03-11 11:53:57 +0800

    Ignore master branch for now.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build/subsplit-basic.sh
+++ build/subsplit-basic.sh
@@ -6,5 +6,5 @@ else
     git subsplit init git@github.com:jomweb/platform.git
 fi
 
-git subsplit publish --heads="master develop" --no-tags extensions/diskus:git@github.com:orchestral/diskus.git
-git subsplit publish --heads="master develop" --no-tags extensions/shorty:git@github.com:orchestral/shorty.git
+git subsplit publish --heads="develop" --no-tags extensions/diskus:git@github.com:orchestral/diskus.git
+git subsplit publish --heads="develop" --no-tags extensions/shorty:git@github.com:orchestral/shorty.git
