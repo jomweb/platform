169aa8a71763194ec3001795b099ca2fe2fc38dd
crynobone <crynobone@gmail.com>
2015-07-28 21:26:36 +0800

    Fixes syntax.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -4,6 +4,7 @@ use Orchestra\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
+    /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array
